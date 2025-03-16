572b6798e72f3cbb0e7cacace806c4f2329a21aa
Raghad <91997030+Ghaadah@users.noreply.github.com>
2025-03-16 01:56:30 -0700

    Update Homepage.jsx

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
 
+const API_URL = import.meta.env.VITE_API_URL;
 const Homepage = () => {
   const [categories, setCategories] = useState([]);
   const [words, setWords] = useState([]);
