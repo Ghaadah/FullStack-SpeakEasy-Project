bc929718f38c307ab857f6a309f9fe21731266b0
Raghad Al Musawi <raghad3@pdx.edu>
2025-03-16 12:47:24 -0700

    adding frontend test folder

--- api/api.js
+++ api/api.js
@@ -1,19 +1,20 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import process from "process";
-import dotenv from "dotenv";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import process from 'process';
+import dotenv from 'dotenv';
 
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-dotenv.config({ path: "../.env" });
+dotenv.config({ path: '../.env' });
 
 const API_KEY = process.env.API_KEY;
 const PROJECT_URL = process.env.PROJECT_URL;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,28 +23,28 @@ app.use(cors());
 
 const supabase = createClient(PROJECT_URL, API_KEY);
 
-app.get("/words", async (req, res) => {
-  const { data, error } = await supabase.from("words").select();
+app.get('/words', async (req, res) => {
+  const { data, error } = await supabase.from('words').select();
   if (error) return res.status(500).json({ error: error.message });
   res.json(data);
 });
 
-app.get("/categories", async (req, res) => {
-  const { data, error } = await supabase.from("categories").select();
+app.get('/categories', async (req, res) => {
+  const { data, error } = await supabase.from('categories').select();
   if (error) return res.status(500).json({ error: error.message });
   res.json(data);
 });
 
-app.post("/categories", async (req, res) => {
-  const { data, error } = await supabase.from("categories").insert({
+app.post('/categories', async (req, res) => {
+  const { data, error } = await supabase.from('categories').insert({
     name: req.body.name,
   });
   if (error) return res.status(500).json(error);
-  res.json({ message: "Successfully added category", addedCategory: data });
+  res.json({ message: 'Successfully added category', addedCategory: data });
 });
 
-app.post("/words", async (req, res) => {
-  const { data, error } = await supabase.from("words").insert({
+app.post('/words', async (req, res) => {
+  const { data, error } = await supabase.from('words').insert({
     word: req.body.word,
     image: req.body.image,
     category: req.body.category,
@@ -51,15 +52,15 @@ app.post("/words", async (req, res) => {
     isCategoryId: req.body.isCategoryId,
   });
   if (error) return res.send(error);
-  res.json({ message: "Successfully added word", addedWord: data });
+  res.json({ message: 'Successfully added word', addedWord: data });
 });
 
-app.put("/words/:id", async (req, res) => {
+app.put('/words/:id', async (req, res) => {
   const id = parseInt(req.params.id, 10);
-  if (isNaN(id)) return res.status(400).json({ error: "Invalid ID format" });
+  if (isNaN(id)) return res.status(400).json({ error: 'Invalid ID format' });
 
   const { data, error } = await supabase
-    .from("words")
+    .from('words')
     .update({
       word: req.body.word,
       image: req.body.image,
@@ -67,38 +68,38 @@ app.put("/words/:id", async (req, res) => {
       isCategory: req.body.isCategory,
       isCategoryId: req.body.isCategoryId,
     })
-    .eq("id", id);
+    .eq('id', id);
 
   if (error) return res.status(500).json(error);
 
   if (data.length === 0)
-    return res.status(404).json({ error: "Word not found" });
+    return res.status(404).json({ error: 'Word not found' });
 
-  res.json({ message: "Successfully updated word", updatedWord: data });
+  res.json({ message: 'Successfully updated word', updatedWord: data });
 });
 
-app.delete("/words/:id", async (req, res) => {
+app.delete('/words/:id', async (req, res) => {
   const id = parseInt(req.params.id, 10);
-  if (isNaN(id)) return res.status(400).json({ error: "Invalid ID format" });
+  if (isNaN(id)) return res.status(400).json({ error: 'Invalid ID format' });
 
   const { data, error } = await supabase
-    .from("words")
+    .from('words')
     .delete()
-    .eq("id", id)
+    .eq('id', id)
     .select();
 
   if (error) return res.status(500).json(error);
 
   if (data.length === 0)
-    return res.status(404).json({ error: "Word not found" });
+    return res.status(404).json({ error: 'Word not found' });
 
-  res.json({ message: "Word successfully deleted", deletedWord: data });
+  res.json({ message: 'Word successfully deleted', deletedWord: data });
 });
 
-app.get("/", (req, res) => {
-  res.send("SpeakEasy API...");
+app.get('/', (req, res) => {
+  res.send('SpeakEasy API...');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`> Ready on http://localhost:3000`);
 });
